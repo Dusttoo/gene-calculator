5504a37360667180ec30d86cef382efb2e8b25f8
Dusty Mumphrey <dusty@clockwork.com>
2023-08-10 10:52:36 -0500

    set production proxy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -32,11 +32,14 @@ function App() {
     });
 
     try {
-      const res = await fetch(`/api/gene-calculator`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(listOfGenes),
-      });
+      const res = await fetch(
+        `${process.env.REACT_APP_API_URL}/api/gene-calculator`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(listOfGenes),
+        }
+      );
       const data = await res.json();
       console.log(data);
       setGeneResults(data);
